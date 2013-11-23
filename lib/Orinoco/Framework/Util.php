91131074e82cbf1ea020d4d55cd7645d7d58a529
Ryan Yonzon <rawswift@gmail.com>
2013-11-23 10:36:47 +0800

    Add Utility class and Orinoco\Framework\Controller::redirect() method.

---
+++ lib/Orinoco/Framework/Util.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Orinoco Framework - A lightweight PHP framework.
+ *  
+ * Copyright (c) 2008-2013 Ryan Yonzon, http://www.ryanyonzon.com/
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace Orinoco\Framework;
+
+/**
+ * Set of methods that perform common, often re-used functions.
+ * Utility classes define these common methods under static scope.
+ */
+class Util
+{
+    /**
+     * This is just a sample method that returns bool true, nothing useful
+     */
+    public function sampleMethod()
+    {
+        return true;
+    }
+}
